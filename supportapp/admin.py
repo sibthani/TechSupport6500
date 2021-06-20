aa555876b3558c954eeb7b0243532bdf37eaf549
L3VyM <lmbewe2016@gmail.com>
2021-06-19 18:15:30 -0700

    Updated models and added new ones

--- supportapp/admin.py
+++ supportapp/admin.py
@@ -7,20 +7,28 @@ from .forms import *
 from .models import *
 
 
+
+admin.site.register(Cluster)
+admin.site.register(Center)
+admin.site.register(Staff)
+admin.site.register(Issue)
+admin.site.register(Issue_Catgry)
+
 class CustomUserAdmin(UserAdmin):
     model = CoachUser
     list_display = ('username','first_name', 'is_staff', 'is_active',)
     list_filter = ('username','first_name','is_staff', 'is_active',)
     fieldsets = (
         (None, {'fields': ('username','email', 'first_name','last_name','password','country_code'
-        ,'cluster_code','center_code','coach_career','address','mobile_money','groups')}),
+        ,'cluster_code','center_code','coach_career','address','phone_number','groups')}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
             'fields': ('username','password1','password2','email', 
-            'first_name','last_name','gender',
+            'first_name','last_name','gender','phone_number','cluster_code','center_code',
+            'staff_code',
             'is_staff', 'is_active')}
         ),
     )
--- supportapp/models.py
+++ supportapp/models.py
@@ -4,9 +4,7 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
-
 from .managers import CustomUserManager
-
 import uuid
 
 # Create your models here.
@@ -16,15 +14,14 @@ class CoachUser(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(auto_now_add=True)
-    #coach_career = models.ForeignKey("careerconfig", on_delete=models.CASCADE,null=True)
+    staff_code = models.ForeignKey("Staff", on_delete=models.CASCADE,null=True)
     first_name = models.CharField(max_length=50,null=True)
     last_name = models.CharField(max_length=50,null=True)
     gender = models.CharField(max_length=6,null=True)
-    #country_code = models.ForeignKey("countryconfig", on_delete=models.CASCADE,null=True)
-    #cluster_code = models.ForeignKey("clusterconfig", on_delete=models.CASCADE, null=True)
-    #center_code = models.ForeignKey("centerconfig", on_delete=models.CASCADE, null=True)
+    cluster_code = models.ForeignKey("Cluster", on_delete=models.CASCADE, null=True)
+    center_code = models.ForeignKey("Center", on_delete=models.CASCADE, null=True)
     address = models.CharField(max_length=254)
-    mobile_money = models.IntegerField(null=True)
+    phone_number = models.IntegerField(null=True)
     registration_date = models.DateField(auto_now_add=True)
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = []
@@ -33,3 +30,64 @@ class CoachUser(AbstractBaseUser, PermissionsMixin):
     
     def __str__(self):
         return f"{self.username}"
+
+
+"""
+The cluster define a defined work portion which has locations called Centers
+The model below defines that
+"""
+
+class Cluster(models.Model):
+    cluster_code = models.CharField(max_length = 3, primary_key=True)
+    cluster_name = models.CharField(max_length=50)
+    province = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.cluster_code}"
+
+# Center is a subset of a cluster, FK in the model is PK in Cluster model
+class Center(models.Model):
+    cluster_code = models.ForeignKey("Cluster", on_delete=models.CASCADE)
+    center_code = models.CharField(max_length=3, primary_key=True)
+    center_name = models.CharField(max_length=50)
+    center_address = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.center_code}"
+
+
+"""
+Defines Staff credentials and other issues that pertain to particular staff
+"""
+class Staff(models.Model):
+    staff_code = models.CharField(max_length=3, primary_key=True)
+    career_descrptn = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.staff_code}"
+
+
+class Issue_Catgry(models.Model):
+    issue_code = models.CharField(max_length=30, primary_key=True)
+    description = models.CharField(max_length=150)
+
+
+    def __str__(self):
+        return f"{self.issue_code}"
+
+
+# Issues are defined by codes LPTOP - Laptop issues
+class Issue(models.Model):
+    issue_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    issue_code = models.ForeignKey("Issue_Catgry", on_delete=models.CASCADE)
+    username = models.ForeignKey("CoachUser", on_delete=models.CASCADE)
+    cluster_code = models.ForeignKey("Cluster", on_delete=models.CASCADE, null=True)
+    center_code = models.ForeignKey("Center", on_delete=models.CASCADE, null=True)
+    issue_description = models.TextField(max_length=150)
+    urgent = models.CharField(max_length=10)
+    issue_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.issue_id}"
+
+    
